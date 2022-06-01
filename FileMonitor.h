b6294f0674f68bd16bc42b75fa5edd889740e34f
Lin <linasharko59612@gmail.com>
2022-06-02 03:11:07 +0700

    Добавлены структура FileState, классы FileObserver, ConsoleFileObserver
    структура FileState - структура для отслеживания состояния конкретного файла
    класс FileObserver - интерфейсный класс Наблюдатель
    класс ConsoleFileObserver - класс Наблюдатель

---
+++ FileMonitor.h
@@ -0,0 +1,5 @@
+#ifndef FILEMONITOR_H
+#define FILEMONITOR_H
+#include "FileObserver.h"
+
+#endif // FILEMONITOR_H
---
+++ Observable.h
@@ -0,0 +1,6 @@
+#ifndef OBSERVABLE_H
+#define OBSERVABLE_H
+
+#include <set>
+#include <vector>
+#endif // OBSERVABLE_H
