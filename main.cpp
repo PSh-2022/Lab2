8b2375b988bc43ff17df91a76d9fda9a27fcd024
Lin <linasharko59612@gmail.com>
2022-06-06 03:30:49 +0700

    Добавлены и завершены классы FileMonitor, Observable. Написан main.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-
+#include "FileMonitor.h"
+#include "FileObserver.h"
+#include "Observable.h"
 int main() {
-
+    FileMonitor monitor( "test.txt" );//содаем объект-источник для отслеживания текстового файла
+    ConsoleFileObserver observer;//создаем объект-Наблюдатель
+    monitor.registerObserver( &observer );//связываем Наблюдателя с источником
+    while( true ) {
+        //обновляем состояние Источника каждые 300 миллисекунд
+        monitor.checkFile();
+        std::this_thread::sleep_for( std::chrono::milliseconds( 300 ) );
+    }
     return 0;
 }
+/*Команды в консоли:
+ * 1. при запуске - файл не существует
+ * 2. touch test.txt - есть пустой файл
+ * 3. echo "TEST" >> test.txt - файл существует, не пуст*/
